41e1368a99a511447c143b55bc40c88400d3912e
Martín Calderón <martincalderon92@outlook.com>
2021-11-05 19:45:29 -0300

    :art: Improve Characters page.

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import styles from '../styles/Characters.module.scss';
-// import useFetch from '../hooks/useFetch';
-import data from '../jsons/characters.json';
-import Card from '../components/Card';
+import useFetch from '../hooks/useFetch';
+// import data from '../jsons/characters.json';
+import CardsContainer from '../components/CardsContainer';
+import PaginationButtons from '../components/PaginationButtons';
 
 export default function Characters() {
-	// const fetchUrl = `https://gateway.marvel.com:443/v1/public/characters`;
-	// const { data, loading } = useFetch(fetchUrl);
+	const fetchUrl = `https://gateway.marvel.com:443/v1/public/characters`;
+	const { data, loading } = useFetch(fetchUrl);
+	const postsPerPage = 8;
+
+	const handlePaginate = (pageNumber: number) => {
+		console.log(pageNumber);
+	};
+
+	const isCharactersData = (data: any) => {
+		return data?.data && data?.data?.results;
+	};
+
+	const hasTotal = (data: any) => {
+		return data?.data && data?.data?.total;
+	};
 
 	return (
 		<div className={styles.characters}>
-			{data &&
-				data?.data?.results?.map((character) => (
-					<Card
-						key={`character${character.id}`}
-						id={character.id}
-						name={character.name}
-						thumbnail={
-							character.thumbnail?.path + '.' + character.thumbnail?.extension
-						}
-					/>
-				))}
+			<CardsContainer loading={loading} posts={isCharactersData(data)} />
+
+			<PaginationButtons
+				totalPosts={hasTotal(data)}
+				postsPerPage={postsPerPage}
+				paginate={handlePaginate}
+			/>
 		</div>
 	);
 }
