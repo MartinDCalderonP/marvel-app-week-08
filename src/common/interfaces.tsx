b19a5d9528b084c19de8c64d7b3d86e7119a4d60
Martín Calderón <martincalderon92@outlook.com>
2021-11-05 19:45:14 -0300

    🏷️ Add ICharacter interface.

--- src/common/interfaces.tsx
+++ src/common/interfaces.tsx
@@ -18,7 +18,7 @@ export interface IUseFetch<T> {
 export interface ICardsContainer {
 	className?: string;
 	loading: boolean;
-	posts: any;
+	posts: ICharacter[];
 }
 
 export interface IPaginationButtons {
@@ -31,4 +31,58 @@ export interface ICard {
 	id: number;
 	name: string;
 	thumbnail: string;
+}
+
+export interface ICharacter {
+	id: number;
+	name: string;
+	description: string;
+	modified: string;
+	thumbnail: Thumbnail;
+	resourceURI: string;
+	comics: Comics;
+	series: Comics;
+	stories: Stories;
+	events: Comics;
+	urls: URL[];
+}
+
+export interface Comics {
+	available: number;
+	collectionURI: string;
+	items: ComicsItem[];
+	returned: number;
+}
+
+export interface ComicsItem {
+	resourceURI: string;
+	name: string;
+}
+
+export interface Stories {
+	available: number;
+	collectionURI: string;
+	items: StoriesItem[];
+	returned: number;
+}
+
+export interface StoriesItem {
+	resourceURI: string;
+	name: string;
+	type: Type;
+}
+
+export enum Type {
+	Cover = 'cover',
+	InteriorStory = 'interiorStory',
+}
+
+export interface Thumbnail {
+	path: string;
+	extension: string;
+}
+
+export interface URL {
+	type: string;
+	url: string;
 }
