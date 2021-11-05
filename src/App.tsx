4ba5dc78d0d586e8208cfb73eead0935a38f64a9
Martín Calderón <martincalderon92@outlook.com>
2021-11-05 14:18:55 -0300

    :sparkles: Add custom Route with Footer and Redirect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 // import { useCounter } from './context/Context';
-// import RouteWithFooter from './components/RouteWithFooter';
-import Navbar from './components/Navbar';
+import RouteWithFooter from './components/RouteWithFooter';
 import Home from './pages/Home';
-import Footer from './components/Footer';
 
 export default function App() {
 	// const { state, dispatch } = useCounter();
 
 	return (
 		<Router>
-			<Navbar />
+			<Switch>
+				<RouteWithFooter path="/">
+					<Home />
+				</RouteWithFooter>
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-
-				<Navigate to={{ pathname: '/' }} />
-			</Routes>
-
-			<Footer />
+				<Redirect to={{ pathname: '/' }} />
+			</Switch>
 		</Router>
 	);
 }
