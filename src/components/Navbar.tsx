21dc9f4ccdd3a0693450dccf4750e8fb3cd9e602
Martín Calderón <martincalderon92@outlook.com>
2021-11-05 21:43:07 -0300

    :sparkles: Add NavLink to Home in Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.scss';
+import { paths } from '../common/enums';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<NavLink
-				to="/characters"
+				to={paths.home}
+				className={(isActive) => (isActive ? styles.active : '')}
+			>
+				<FontAwesomeIcon className={styles.homeIcon} icon={faHome} />
+				Home
+			</NavLink>
+
+			<NavLink
+				to={paths.characters}
 				className={(isActive) => (isActive ? styles.active : '')}
 			>
 				Characters
 			</NavLink>
 
 			<NavLink
-				to="/comics"
+				to={paths.comics}
 				className={(isActive) => (isActive ? styles.active : '')}
 			>
 				Comics
 			</NavLink>
 
 			<NavLink
-				to="/stories"
+				to={paths.stories}
 				className={(isActive) => (isActive ? styles.active : '')}
 			>
 				Stories
