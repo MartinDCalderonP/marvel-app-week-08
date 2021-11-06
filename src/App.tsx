46358a5246b1e1bcae9dac59c6f354e4bcb9be71
Martín Calderón <martincalderon92@outlook.com>
2021-11-06 11:12:00 -0300

    :recycle: Change Custom Route to from with Footer to with Search.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 // import { useCounter } from './context/Context';
 import { paths } from './common/enums';
-import RouteWithFooter from './components/RouteWithFooter';
+import RouteWithSearch from './components/RouteWithSearch';
 import Home from './pages/Home';
 import Characters from './pages/Characters';
 
@@ -12,13 +17,19 @@ export default function App() {
 	return (
 		<Router>
 			<Switch>
-				<RouteWithFooter exact path={paths.home}>
+				<Route exact path={paths.home}>
 					<Home />
-				</RouteWithFooter>
+				</Route>
 
-				<RouteWithFooter path={`${paths.characters}${paths.page}=:page`}>
+				<RouteWithSearch path={`${paths.characters}${paths.page}=:page`}>
 					<Characters />
-				</RouteWithFooter>
+				</RouteWithSearch>
+
+				<RouteWithSearch
+					path={`${paths.search}:searchedTerm${paths.page}=:page`}
+				>
+					<Characters />
+				</RouteWithSearch>
 
 				<Redirect to={{ pathname: `${paths.home}` }} />
 			</Switch>
--- src/components/RouteWithSearch.tsx
+++ src/components/RouteWithSearch.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { ICustomRoute } from '../common/interfaces';
 import Navbar from './Navbar';
+import SearchInput from './SearchInput';
 import Footer from './Footer';
 
-export default function RouteWithFooter({ children, ...rest }: ICustomRoute) {
+export default function RouteWithSearch({ children, ...rest }: ICustomRoute) {
 	return (
 		<Route {...rest}>
 			<Navbar />
+			<SearchInput />
 			{children}
-			<Footer />{' '}
+			<Footer />
 		</Route>
 	);
 }
