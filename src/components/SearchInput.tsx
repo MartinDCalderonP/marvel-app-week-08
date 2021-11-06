dd2c0eb6997c9f282f86d7167c63110b4c131ca3
Martín Calderón <martincalderon92@outlook.com>
2021-11-06 17:48:33 -0300

    ⚡️ Disable Suggestions List.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,21 +1,22 @@
-import React, { useState, useCallback, ChangeEvent, MouseEvent } from 'react';
+import React, { useState, ChangeEvent, MouseEvent } from 'react';
+// ^ useCallback
 import styles from '../styles/SearchInput.module.scss';
 import { useHistory } from 'react-router-dom';
-import _ from 'lodash';
+// import _ from 'lodash';
 import { paths } from '../common/enums';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import SuggestionsList from './SuggestionsList';
+// import SuggestionsList from './SuggestionsList';
 
 export default function SearchInput() {
 	const [searchedTerm, setSearchedTerm] = useState<string>('');
-	const [openSuggestions, setOpenSuggestions] = useState<boolean>(false);
-	const [pressedKey, setPressedKey] = useState<number>(0);
+	// const [openSuggestions, setOpenSuggestions] = useState<boolean>(false);
+	// const [pressedKey, setPressedKey] = useState<number>(0);
 	const history = useHistory();
 
 	const handleSearchedTermChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setSearchedTerm(e.target.value);
-		setOpenSuggestions(true);
+		// setOpenSuggestions(true);
 	};
 
 	const searchTerm = (term: string) => {
@@ -30,26 +31,26 @@ export default function SearchInput() {
 		}
 	};
 
-	const handleSuggestionSelected = (suggestionSelected: string) => {
-		setSearchedTerm(suggestionSelected);
-		searchTerm(suggestionSelected);
-	};
+	// const handleSuggestionSelected = (suggestionSelected: string) => {
+	// 	setSearchedTerm(suggestionSelected);
+	// 	searchTerm(suggestionSelected);
+	// };
 
-	const handleCloseSuggestions = () => {
-		setOpenSuggestions(false);
-	};
+	// const handleCloseSuggestions = () => {
+	// 	setOpenSuggestions(false);
+	// };
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const debouncePressKey = useCallback(
-		_.debounce((e) => {
-			setPressedKey(e);
-		}, 500),
-		[setPressedKey]
-	);
+	// const debouncePressKey = useCallback(
+	// 	_.debounce((e) => {
+	// 		setPressedKey(e);
+	// 	}, 500),
+	// 	[setPressedKey]
+	// );
 
-	const handleKeyNavigation = (e: any) => {
-		debouncePressKey(e);
-	};
+	// const handleKeyNavigation = (e: any) => {
+	// 	debouncePressKey(e);
+	// };
 
 	return (
 		<form className={styles.searchForm} autoComplete="off">
@@ -57,7 +58,7 @@ export default function SearchInput() {
 				<input
 					value={searchedTerm}
 					onChange={handleSearchedTermChange}
-					onKeyDown={handleKeyNavigation}
+					// onKeyDown={handleKeyNavigation}
 					type="text"
 					name="search"
 					placeholder="Search"
@@ -71,14 +72,14 @@ export default function SearchInput() {
 				</button>
 			</div>
 
-			{openSuggestions && (
+			{/* {openSuggestions && (
 				<SuggestionsList
 					searchedTerm={searchedTerm}
 					suggestionSelected={handleSuggestionSelected}
 					closeSuggestions={handleCloseSuggestions}
 					pressedKey={pressedKey}
 				/>
-			)}
+			)} */}
 		</form>
 	);
 }
