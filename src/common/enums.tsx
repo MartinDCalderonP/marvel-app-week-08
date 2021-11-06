929fd24baa6edbbec43a35d118b32a4e2a8e4a0a
Martín Calderón <martincalderon92@outlook.com>
2021-11-06 10:56:27 -0300

    :recycle: Fix search path.

--- src/common/enums.tsx
+++ src/common/enums.tsx
@@ -1,7 +1,7 @@
 export enum paths {
 	home = '/home',
 	page = '/page',
-	search = '/?search_query=',
+	search = '/results?search_query=',
 	characters = '/characters',
 	comics = '/comics',
 	stories = '/stories',
