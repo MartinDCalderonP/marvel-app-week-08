20187b4b564e9206e8b630499a4241aee992d0dd
Martín Calderón <martincalderon92@outlook.com>
2021-11-08 07:48:19 -0300

    :recycle: Improve Select.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Select.module.scss';
-import { API } from '../common/enums';
+import { useHistory, useLocation } from 'react-router';
+import { paths, API } from '../common/enums';
 import { ISelect, IComic, IStory } from '../common/interfaces';
 import { isCorrectData } from '../common/typeGuards';
 import useFetch from '../hooks/useFetch';
@@ -10,11 +11,34 @@ export default function Select({ comics, stories }: ISelect) {
 		? `${API.comicsUrl}?${API.limit}10`
 		: `${API.storiesUrl}?${API.limit}10`;
 	const { data, loading } = useFetch(fetchUrl);
+	const [selectedValue, setSelectedValue] = useState<string>('');
+	const history = useHistory();
+	const location = useLocation();
+
+	const handleSelectValueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedValue(e.target.value);
+
+		let newUrl = '';
+
+		if (comics) {
+			newUrl = `${location.pathname}${paths.comics}${e.target.value}`;
+		}
+
+		if (stories) {
+			newUrl = `${location.pathname}${paths.stories}${e.target.value}`;
+		}
+
+		history.push(newUrl);
+	};
 
 	return (
 		<>
 			{!loading && (
-				<select className={styles.select}>
+				<select
+					className={styles.select}
+					value={selectedValue}
+					onChange={handleSelectValueChange}
+				>
 					<option hidden>Select a {comics ? 'Comic' : 'Story'}.</option>
 
 					{isCorrectData(data).length > 0 &&
