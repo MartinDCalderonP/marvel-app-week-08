4930c68d3895b6e2eec88b28e480f6d3e756dd28
Martín Calderón <martincalderon92@outlook.com>
2021-11-08 16:27:57 -0300

    :art: Add some info to Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,15 @@ export default function Home() {
 				src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg"
 				alt="Logo"
 			/>
+			
+			<p>
+				Marvel Entertainment, LLC, a wholly-owned subsidiary of The Walt Disney
+				Company, is one of the world's most prominent character-based
+				entertainment companies, built on a proven library of more than 8,000
+				characters featured in a variety of media over seventy-five years.
+				Marvel utilizes its character franchises in entertainment, licensing and
+				publishing. For more information visit marvel.com. © 2020 MARVEL
+			</p>
 		</div>
 	);
 }
