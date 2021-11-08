b382ec7aa971a355f7714632c9b6a9fa06640a0d
Martín Calderón <martincalderon92@outlook.com>
2021-11-08 10:32:14 -0300

    :fire: Delete unused vars on Characters.

--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -5,21 +5,18 @@ import { paths, API } from '../common/enums';
 import { IUseParams } from '../common/interfaces';
 import { isCorrectData, hasTotal } from '../common/typeGuards';
 import useFetch from '../hooks/useFetch';
-// import data from '../jsons/characters.json';
 import Spinner from '../components/Spinner';
 import CardsContainer from '../components/CardsContainer';
 import PaginationButtons from '../components/PaginationButtons';
 
 export default function Characters() {
-	const { page, searchedTerm, comics, stories } = useParams<IUseParams>();
+	const { page, searchedTerm } = useParams<IUseParams>();
 	const [currentPage, setCurrentPage] = useState<number>(parseInt(page));
 	const postsPerPage = 8;
 	const offset = postsPerPage * (currentPage - 1);
 	const fetchUrl =
 		`${API.charactersUrl}?${API.limit}${postsPerPage}&${API.offset}${offset}` +
-		(searchedTerm ? `&${API.search}${searchedTerm}` : '') +
-		(comics ? `&${API.comics}${comics}` : '') +
-		(stories ? `&${API.stories}${stories}` : '');
+		(searchedTerm ? `&${API.search}${searchedTerm}` : '');
 	const { data, loading } = useFetch(fetchUrl);
 	const history = useHistory();
 
@@ -30,14 +27,6 @@ export default function Characters() {
 
 		if (searchedTerm) {
 			newUrl = `${paths.search}${searchedTerm}${paths.page}${pageNumber}`;
-		}
-
-		if (comics) {
-			newUrl = `${paths.comics}${comics}${paths.page}${pageNumber}`;
-		}
-
-		if (stories) {
-			newUrl = `${paths.stories}${stories}${paths.page}${pageNumber}`;
 		}
 
 		history.push(newUrl);
